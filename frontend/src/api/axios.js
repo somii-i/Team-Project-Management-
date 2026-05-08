28238f4c67853279922c448da47d7d6b5ed52f69
K Somiya <ksomiya73@gmail.com>
2026-05-08 21:22:34 +0530

    your message

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://team-project-management-g3u2.onrender.com',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 });
 
 instance.interceptors.request.use(config => {
